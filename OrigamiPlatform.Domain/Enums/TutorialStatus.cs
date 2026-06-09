c3d201389b102534843f08fc6b6bdaae7513d0e9
TrungNHT03 <trungnht1923@gmail.com>
2026-06-10 02:49:57 +0700

    Upodate tutorials status

---
+++ OrigamiPlatform.Domain/Enums/TutorialStatus.cs
@@ -0,0 +1,13 @@
+namespace OrigamiPlatform.Domain.Enums;
+
+public enum TutorialStatus
+{
+    Draft,
+    PendingCTVReview,
+    PendingManagerReview,
+    RevisionRequired,
+    Published,
+    Rejected,
+    Removed,
+    EditPendingReview
+}
