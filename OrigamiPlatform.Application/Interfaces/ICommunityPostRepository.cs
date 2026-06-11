2b08a5b5ade1345e1339510bc55253a3dc639f18
LeLoc1712 <levuvinhloc.vn@gmail.com>
2026-06-11 16:31:59 +0700

    Update repositories

---
+++ OrigamiPlatform.Application/Interfaces/ICommunityPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrigamiPlatform.Domain.Entities;
+
+namespace OrigamiPlatform.Application.Interfaces
+{
+    public interface ICommunityPostRepository
+    {
+        Task<CommunityPost> AddAsync(CommunityPost post);
+        Task<CommunityPost?> GetByIdAsync(Guid id);
+        // Tạm thời chuẩn bị 2 hàm này, các hàm get list sẽ bổ sung sau khi làm Query
+    }
+}
---
+++ OrigamiPlatform.Application/Interfaces/ILikeRepository.cs
@@ -0,0 +1,13 @@
+﻿using OrigamiPlatform.Domain.Entities;
+using OrigamiPlatform.Domain.Enums;
+
+namespace OrigamiPlatform.Application.Interfaces
+{
+    public interface ILikeRepository
+    {
+        // Kiểm tra xem User đã like mục này chưa
+        Task<Like?> GetLikeAsync(Guid userId, Guid targetId, TargetType targetType);
+        Task AddAsync(Like like);
+        Task RemoveAsync(Like like);
+    }
+}
