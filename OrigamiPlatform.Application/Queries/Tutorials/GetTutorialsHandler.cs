c4c54bb3fe23303fc5be535f85cc9948d7723051
LeLoc1712 <levuvinhloc.vn@gmail.com>
2026-06-28 17:58:23 +0700

    Fix get comment, like, wishlist

--- OrigamiPlatform.Application/Queries/Tutorials/GetTutorialsHandler.cs
+++ OrigamiPlatform.Application/Queries/Tutorials/GetTutorialsHandler.cs
@@ -11,12 +11,25 @@ public class GetTutorialsHandler
     private readonly ITutorialRepository _tutorials;
     private readonly IVipSubscriptionRepository _vipSubscriptions;
     private readonly IUserRepository _users;
+    private readonly ILikeRepository _likes;
+    private readonly IWishlistRepository _wishlists;
+    private readonly ICommentRepository _comments;
 
     public GetTutorialsHandler(
-        ITutorialRepository tutorials,
-        IVipSubscriptionRepository vipSubscriptions,
-        IUserRepository users)
-        => (_tutorials, _vipSubscriptions, _users) = (tutorials, vipSubscriptions, users);
+         ITutorialRepository tutorials,
+         IVipSubscriptionRepository vipSubscriptions,
+         IUserRepository users,
+         ILikeRepository likes,
+         IWishlistRepository wishlists,
+         ICommentRepository comments)
+    {
+        _tutorials = tutorials;
+        _vipSubscriptions = vipSubscriptions;
+        _users = users;
+        _likes = likes;
+        _wishlists = wishlists;
+        _comments = comments;
+    }
 
     public async Task<PagedResult<TutorialListItemDto>> HandleAsync(
         GetTutorialsQuery query, CancellationToken ct = default)
@@ -49,6 +62,30 @@ public class GetTutorialsHandler
         var (items, totalCount) = await _tutorials.GetPublishedAsync(
             query.Search, query.CategoryId, query.Difficulty, type, sortBy, page, pageSize, followedIds, ct);
 
+        var likeCounts = new Dictionary<Guid, int>();
+        var wishlistCounts = new Dictionary<Guid, int>();
+        var commentCounts = new Dictionary<Guid, int>();
+        var userLikedPostIds = new HashSet<Guid>();
+        var userWishlistedPostIds = new HashSet<Guid>();
+
+        foreach (var t in items)
+        {
+            likeCounts[t.Id] = await _likes.GetLikeCountAsync(t.Id, TargetType.Tutorial);
+            wishlistCounts[t.Id] = await _wishlists.GetWishlistCountAsync(t.Id, TargetType.Tutorial, ct);
+            commentCounts[t.Id] = await _comments.GetCommentCountAsync(t.Id, TargetType.Tutorial, ct); 
+
+            if (query.CurrentUserId.HasValue)
+            {
+                var userId = query.CurrentUserId.Value;
+
+                if (await _likes.GetLikeAsync(userId, t.Id, TargetType.Tutorial) != null)
+                    userLikedPostIds.Add(t.Id);
+
+                if (await _wishlists.GetByUserAndTargetAsync(userId, t.Id, TargetType.Tutorial, ct) != null)
+                    userWishlistedPostIds.Add(t.Id);
+            }
+        }
+
         var dtos = items.Select(t => new TutorialListItemDto(
             t.Id,
             t.Title,
@@ -65,7 +102,13 @@ public class GetTutorialsHandler
                 t.Author.Profile?.AvatarUrl),
             t.Steps.Count,
             t.PublishedAt!.Value,
-            IsVipLocked: t.Type == TutorialType.VIP && !subscribedCreatorIds.Contains(t.Author.Id)
+            IsVipLocked: t.Type == TutorialType.VIP && !subscribedCreatorIds.Contains(t.Author.Id),
+
+            LikeCount: likeCounts.GetValueOrDefault(t.Id, 0),
+            WishlistCount: wishlistCounts.GetValueOrDefault(t.Id, 0),
+            CommentCount: commentCounts.GetValueOrDefault(t.Id, 0),
+            IsLikedByCurrentUser: userLikedPostIds.Contains(t.Id),
+            IsWishlistedByCurrentUser: userWishlistedPostIds.Contains(t.Id)
         )).ToList();
 
         return new PagedResult<TutorialListItemDto>(
