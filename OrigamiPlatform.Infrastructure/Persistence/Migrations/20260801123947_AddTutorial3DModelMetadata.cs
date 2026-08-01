384be6b90e22bf1a108a74947cc6e7bf6e83dc4d
ThaiTDHE173054 <ThaiTDHE173054@fpt.edu.vn>
2026-08-01 19:41:43 +0700

    feat(ft-04): add tutorial 3d model metadata

---
+++ OrigamiPlatform.Infrastructure/Persistence/Migrations/20260801123947_AddTutorial3DModelMetadata.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrigamiPlatform.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTutorial3DModelMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Model3DPosterUrl",
+                table: "Tutorials",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model3DUrl",
+                table: "Tutorials",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model3DPosterUrl",
+                table: "Tutorials");
+
+            migrationBuilder.DropColumn(
+                name: "Model3DUrl",
+                table: "Tutorials");
+        }
+    }
+}
